e3cad3e2c53548d7a693bfe11158e96355170cd8
Gabriel Rossetto <rossetto.2306@gmail.com>
2020-08-07 09:19:28 -0300

    dia 3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://miro.medium.com/max/909/1*_iikfMGYF3RH4OZ0yeQYnQ.png" alt="Gabriel Rossetto"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Monika Whitehead</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id metus mi. 
-        <br/><br/>
-        Vivamus elementum diam congue ante lacinia dapibus. Donec aliquam, tellus bibendum bibendum sodales, nisl arcu eleifend neque, ac porta metus nulla quis enim. Cras tempus eros quis mauris luctus scelerisque vitae a orci. Cras molestie ligula sed ex finibus rhoncus.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
